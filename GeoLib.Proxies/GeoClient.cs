51263ad24838d8ec13f4533b728385e0e35ef28f
skhandorin <hsatig@mail.ru>
2017-04-20 01:18:22 +0500

    add GeoLib.Proxies

---
+++ GeoLib.Proxies/GeoClient.cs
@@ -0,0 +1,33 @@
+﻿using GeoLib.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib.Proxies
+{
+    public class GeoClient : ClientBase<IGeoService>, IGeoService
+    {
+        public IEnumerable<string> GetStates(bool primaryOnly)
+        {
+            return Channel
+        }
+
+        public ZipCodeData GetZipInfo(string zip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string zip, int range)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
